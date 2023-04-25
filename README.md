d17afd4856ee143af89387067994117617139afd
Jaquiez <lmaokachow@gmail.com>
2023-04-25 16:08:44 -0400

    parser work

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # What The Shell
 
 A simple shell written in Rust using a tree walk intepreter for the scripting langauge.
+
+## Barebones Grammar
+
+```
+    expression      → "(" expression ")"
+                    | expression binary_operator expression
+                    | expression unary_operator
+                    | command;
+
+    command         → WORD arguments;
+
+    arguments       → WORD*
+                    | flag*  arguments;
+
+    flag            → SHORT_FLAG
+                    | LONG_FLAG;
+
+    SHORT_FLAG      → "-" FLAG_NAME;
+    LONG_FLAG       → "--" FLAG_NAME;
+
+    FLAG_NAME       → WORD;
+
+    STRING          → /".*"/;
+    WORD            → /[A-Za-z0-9_-_.]+/;
+
+    unary_operator  → "&"
+                    | ";";
+    
+    binary_operator → "|"
+                    | ">";
+                    | ">>";
+                    | "<";
+                    | "<<";
+```
--- src/parser.rs
+++ src/parser.rs
@@ -12,23 +12,58 @@ struct Node{
     right: Box<Node>
 }
 
-fn block(mut toks: Vec<Token>){
-    while(toks.len() > 0){
+fn parse_string(){
+
+}
+
+fn parse_flag(){
+
+}
+
+
+fn parse_code(mut toks: Vec<Token>){
+    while toks.len() > 0{
         let tok = toks.remove(0);
         match tok.t_type {
-            TokenType::LEFT_PAREN => {},
-            TokenType::RIGHT_PAREN => {},
-            TokenType::SHORT_FLAG => {},
-            TokenType::LONG_FLAG =>{},
-            TokenType::SEMICOLON => {},
-            TokenType::PIPE =>{}
-            TokenType::REDIR_LEFT =>{},
-            TokenType::DOUBLE_REDIR_LEFT=> {},
-            TokenType::DOUBLE_REDIR_RIGHT =>{},
-            TokenType::REDIR_RIGHT=>{},
-            TokenType::POUND => {}
-            TokenType::STRING => {},
-            TokenType::WORD =>{},
+            TokenType::LEFT_PAREN => {
+                //parse_expression();
+            },
+            TokenType::RIGHT_PAREN => {
+                //end node
+            },
+            TokenType::SHORT_FLAG => {
+                parse_flag();
+            },
+            TokenType::LONG_FLAG =>{
+                parse_flag();
+            },
+            TokenType::SEMICOLON => {
+                //end node!
+            },
+            TokenType::PIPE =>{
+
+            }
+            TokenType::REDIR_LEFT =>{
+
+            },
+            TokenType::DOUBLE_REDIR_LEFT=> {
+                
+            },
+            TokenType::DOUBLE_REDIR_RIGHT =>{
+
+            },
+            TokenType::REDIR_RIGHT=>{
+
+            },
+            TokenType::POUND => {
+
+            },
+            TokenType::STRING => {
+                //treat like word
+            },
+            TokenType::WORD =>{
+
+            },
             _=>{}
         }
         println!("{:#?}",tok);
@@ -39,5 +74,5 @@ fn block(mut toks: Vec<Token>){
 
 pub fn parse_program(lexer: Scanner){
     println!("Entering parser\n{:#?}",lexer.tokens);
-    block(lexer.tokens);
+    parse_code(lexer.tokens);
 }
